d98c2f1d712baaa14ff3dc97b5a89648f9b5e6c2
Niladri <devniladri3@gmail.com>
2026-03-15 00:52:57 +0530

    design the testimonial section make it responsive add sliding functionality in testimonial cards design the page loading feature

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const loadingScreen=document.querySelector(".loading"); 
 const hamMenu=document.querySelector(".menuBtn")
 const sideBar=document.querySelector(".sidebar")
 const callBtn=document.querySelector(".callBtn")
+const prevBtn=document.querySelector(".prev")
+const nextBtn=document.querySelector(".next")
+const slideDots=document.querySelectorAll(".dot")
+
+const slides=document.querySelectorAll(".cardslides")
+
+window.addEventListener('load',()=>{
+     loadingScreen.style.display="none"
+})
+
 hamMenu.addEventListener('click',()=>{
     
    sideBar.classList.toggle("activeBar")
@@ -14,3 +25,79 @@ callBtn.addEventListener('click',()=>{
      window.location.href = `tel:${+916289672155}`;
      console.log("clicked")
 })
+
+let slidePointer=0;
+
+
+const slideTimer = setInterval(()=>{
+
+     if(slidePointer!=slides.length-1){
+          nextSlide()
+     }
+     else{
+          setSlide()
+     }
+},3000)
+nextBtn.addEventListener('click',()=>{
+      clearInterval(slideTimer)
+    if(slidePointer!=slides.length-1){
+          nextSlide(slidePointer)
+    }
+    else{
+          setSlide()
+    }
+     
+})
+
+prevBtn.addEventListener('click',()=>{
+     clearInterval(slideTimer)
+     if(slidePointer!=0){
+          previousSlide()
+     }else{
+          setSlide()
+     }
+     
+})
+
+const nextSlide=()=>{
+     
+          slides[slidePointer].classList.remove("display")
+          slideDots[slidePointer].classList.remove("activeDot")
+          slidePointer++;
+          slides[slidePointer].classList.add("display")
+          slideDots[slidePointer].classList.add("activeDot")
+          
+}
+
+const setSlide=()=>{
+     if(slidePointer!=0){
+          slides[slidePointer].classList.remove("display")
+          slideDots[slidePointer].classList.remove("activeDot")
+          slidePointer=0
+          slides[slidePointer].classList.add("display")
+          slideDots[slidePointer].classList.add("activeDot")
+     }
+     else{
+          slides[slidePointer].classList.remove("display")
+          slideDots[slidePointer].classList.remove("activeDot")
+          slidePointer=slides.length-1
+          slides[slidePointer].classList.add("display")
+          slideDots[slidePointer].classList.add("activeDot")
+     }
+}
+
+const previousSlide=()=>{
+     slides[slidePointer].classList.remove("display")
+     slideDots[slidePointer].classList.remove("activeDot")
+     slidePointer--
+     slides[slidePointer].classList.add("display")
+     slideDots[slidePointer].classList.add("activeDot")
+
+}
+
+
+
+
+
+
+
